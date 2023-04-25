2a267cf6bcc450cdc54c1e56e9e79fa10c17a351
JosipaSaravnja <josipa.saravanja06@gmail.com>
2023-04-26 00:34:55 +0200

    added auth, still have to test

--- Api/Program.cs
+++ Api/Program.cs
@@ -1,12 +1,45 @@
+using System.Text;
+using Api.Constants;
 using Domain;
 using Domain.Middleware;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
 
 var builder = WebApplication.CreateBuilder(args);
-
+var config = builder.Configuration;
 // Add services to the container.
 
 builder.Services.AddApplication();
-
+builder.Services.AddAuthentication(x =>
+{
+    x.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    x.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+    x.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
+}).AddJwtBearer(x =>
+{
+    x.TokenValidationParameters = new TokenValidationParameters
+    {
+        IssuerSigningKey = new SymmetricSecurityKey(
+            Encoding.UTF8.GetBytes(config["Jwt:Key"]!)),
+        ValidateIssuerSigningKey = true,
+        ValidateLifetime = true,
+        ValidIssuer = config["Jwt:Issuer"],
+        ValidAudience = config["Jwt:Audience"],
+        ValidateIssuer = true,
+        ValidateAudience = true
+    };
+});
+builder.Services.AddAuthorization(x =>
+{
+    x.AddPolicy(AuthorizationConstants.AdminUserPolicyName,
+        p => p.RequireClaim(AuthorizationConstants.AdminUserClaimName, "true"));
+    
+    x.AddPolicy(AuthorizationConstants.ValidUserPolicyName,
+        p => p.RequireAssertion(c =>
+            c.User.HasClaim(m => m is { Type: AuthorizationConstants.AdminUserClaimName, Value: "true"}) ||
+            c.User.HasClaim(m => m is { Type: AuthorizationConstants.ValidUserClaimName, Value: "true" })));
+});
+builder.Services.AddAuthorization();
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
